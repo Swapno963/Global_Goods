bd1e17c31d2ecbd69b737f2fcfaad33155427a92
swapno963 <swapno963@gmail.com>
2024-07-12 16:09:25 +0600

    Deprecated problem solved

--- app/api/auth/send_emal/route.js
+++ app/api/auth/send_emal/route.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const config = {
+export const segmentConfig = {
   api: {
     bodyParser: false,
   },
