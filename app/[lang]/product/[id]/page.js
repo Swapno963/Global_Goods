d644058e5d981fde22c816675b5bd6fcd3134de5
swapno963 <swapno963@gmail.com>
2024-08-18 16:03:36 +0600

    related product problem solved

--- app/[lang]/product/[id]/page.js
+++ app/[lang]/product/[id]/page.js
@@ -8,7 +8,7 @@ export default async function page({ params: { id } }) {
   const product = await getProductById(id);
   const session = await auth();
   return (
-    <>
+    <div className="">
       <div className="container py-4 flex items-center gap-3">
         <p className="text-gray-600 font-medium">Product</p>
       </div>
@@ -16,6 +16,6 @@ export default async function page({ params: { id } }) {
       <ProductDetail product={product} session={session} />
       <ProductDescription product={product} />
       <RelatedProducts category_name={product?.category_name} />
-    </>
+    </div>
   );
 }
--- components/product/RelatedProducts.jsx
+++ components/product/RelatedProducts.jsx
@@ -6,11 +6,11 @@ export default async function RelatedProducts({ category_name }) {
 
   return (
     <>
-      <div className="container pb-16">
+      <div className="container pb-12">
         <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
           Related products
         </h2>
-        <div className="grid grid-cols-4 gap-6">
+        <div className="grid grid-cols-4 gap-[60px] ">
           {/* single product */}
           {relatedproduct?.map((pd) => (
             <SingleProduct product={pd} key={pd?.id} />
